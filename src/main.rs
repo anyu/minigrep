38b3f39ff9393e174fc897b48ebdeea975102e66
An Yu <ayu3500@gmail.com>
2021-10-22 16:02:16 -0400

    Add test

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem pasing args: {}", err);
+        println!("Problem parsing arguments {}", err);
         process::exit(1);
     });
     
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        println!("Application Error {}", e);
 
         process::exit(1);
     };
